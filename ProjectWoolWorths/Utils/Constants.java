aca5102bef4753f186ab3b3983785fa85629d931
pntsunts <50695155+pntsunts@users.noreply.github.com>
2021-10-26 14:45:31 +0200

    WorkDone

---
+++ ProjectWoolWorths/Utils/Constants.java
@@ -0,0 +1,7 @@
+package Utils;
+
+public class Constants {
+		  public static final String URL = "https://www.woolworths.co.za/";
+		  public static final String Path_TestData = "C:\\Users\\Peter\\Desktop\\";
+		  public static final String File_TestData = "TestMe.xlsx";
+}
